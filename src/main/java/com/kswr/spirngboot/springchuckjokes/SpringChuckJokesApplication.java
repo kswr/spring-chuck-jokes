831c917c513071d897bafdd19c2354289e337365
kswr <ksawery.oleszynski@gmail.com>
2019-02-13 13:28:32 +0100

    Initial commit

---
+++ src/main/java/com/kswr/spirngboot/springchuckjokes/SpringChuckJokesApplication.java
@@ -0,0 +1,14 @@
+package com.kswr.spirngboot.springchuckjokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChuckJokesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringChuckJokesApplication.class, args);
+    }
+
+}
+
